9400b1a49830c60cca0eaf7596fcf093b413dedc
Rikard Samvik <rav@qlik.com>
2019-10-10 12:16:14 +0200

    images.json

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Flickr from 'flickr-sdk';
 
 function App() {
+
+  const flickr = new Flickr('d74804db9d9e1435539d3f15b5cbfe06');
+  const photos = [];
+  flickr.photos.search({
+    text: 'doggo',
+    per_page: 500
+  }).then(function (res) {
+    console.log(res.body);
+    res.body.photos.photo.forEach((photo) => {
+      flickr.photos.getInfo({photo_id: photo.id}).then( (res) => {
+        console.log(res.body);
+        photos.push(res.body.photo);
+        console.log(JSON.stringify(photos));
+      });
+    });
+  }).catch(function (err) {
+    console.error('bonk', err);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
